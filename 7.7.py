35c8749722e4532061eb567dd3032f7e72063654
csmith14 <CSmith14@protonmail.com>
2017-10-31 18:03:26 -0500

    Update 7.7.py
    - Fixed wrong variable in column1 print statement
    - Added a check for 'comma not followed by integer'

--- 7.7.py
+++ 7.7.py
@@ -1,16 +1,17 @@
+
 title = str(input('Enter a title for the data:\n'))
 print('You entered:', title)
 print()
 
 column1 = str(input('Enter column 1 heading:\n'))
-print('You entered:', title)
+print('You entered:', column1)
 print()
 
 column2 = str(input('Enter column 2 heading:\n'))
 print('You entered:', column2)
 print()
 
-def get_data_points():   #output data points 
+def get_data_points():   #output data points
     data = input('Enter data points:')
     while input != '':
         if data == '-1':
@@ -25,6 +26,13 @@ def get_data_points():   #output data points
             #FIXME: 'Comma not followed by integer' condition
         else:
             data_points = data.split(',')
+            i = 0
+            while i < len(data_points):
+                data_points[i] = data_points[i].strip()
+                i += 1
+            if not data_points[1].isnumeric():
+                print('Error: Comma not followed by an integer.')
+                get_data_points()
             print('Data string:', data_points[0])
             print('Data integer:', data_points[1])
             get_data_points()

eeaaf43c97eabc1d5e485c633e5370eaf04db39a
csmith14 <CSmith14@protonmail.com>
2017-10-31 18:28:20 -0500

    Update 7.7.py
    Added table display function and global arrays for data storage outside get_data_point's scope

--- 7.7.py
+++ 7.7.py
@@ -11,11 +11,15 @@ column2 = str(input('Enter column 2 heading:\n'))
 print('You entered:', column2)
 print()
 
-def get_data_points():   #output data points
-    data = input('Enter data points:')
+data_points_strings = []
+data_points_ints = []
+
+
+def get_data_points():
+    data = input('Enter a data point (-1 to stop input):')
     while input != '':
         if data == '-1':
-            print('Exiting')
+            display_table()
             break
         elif ',' not in data:
             print('Error. No comma in string')
@@ -25,17 +29,30 @@ def get_data_points():   #output data points
             get_data_points()
             #FIXME: 'Comma not followed by integer' condition
         else:
-            data_points = data.split(',')
+            #renamed your data_points array to single_data_points
+            single_data_points = data.split(',')
             i = 0
-            while i < len(data_points):
-                data_points[i] = data_points[i].strip()
+            while i < len(single_data_points):
+                single_data_points[i] = single_data_points[i].strip()
                 i += 1
-            if not data_points[1].isnumeric():
+            if not single_data_points[1].isnumeric():
                 print('Error: Comma not followed by an integer.')
                 get_data_points()
-            print('Data string:', data_points[0])
-            print('Data integer:', data_points[1])
+            print('Data string:', single_data_points[0])
+            print('Data integer:', single_data_points[1])
+            data_points_strings.append(single_data_points[0])
+            data_points_ints.append(single_data_points[1])
             get_data_points()
         return
+
+
+def display_table():
+    print(f'{title:>33}')
+    print(f'{column1:20}|{column2:>23}')
+    print('--------------------------------------------')
+    i = 0
+    while i < len(data_points_ints):
+        print(f'{data_points_strings[i]:20}|{data_points_ints[i]:>23}')
+        i +=1
 
 get_data_points()
